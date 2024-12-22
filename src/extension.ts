2098c7dd825a2720f518d6de88fdeb96b64a3e96
city945 <2654229893@qq.com>
2024-12-22 11:53:47 +0800

    代码清理   - 删除无用代码

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,6 @@ import { FileUtil } from './common/fileUtil';
 
 // 插件激活时执行
 export function activate(context: vscode.ExtensionContext) {
-	// keepOriginDiff();
 	const viewOption = { webviewOptions: { retainContextWhenHidden: true, enableFindWidget: true } };
 	FileUtil.init(context)
 	const markdownService = new MarkdownService(context);
@@ -20,20 +19,3 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 export function deactivate() { }
-
-// @tag delete: 无用代码，功能是如果定义下面的配置，即使用了 gitlens 等插件的 markdown 预览功能，则修改为使用默认的 markdown 编辑器打开，个人不会使用到该配置
-// editorAssociations: [{"filenamePattern": "*.md"...}] settings.json 中的配置项，指定某个类型的文件使用哪个编辑器
-function keepOriginDiff() {
-	// 获取当前工作区的所有配置项，进而获得 editorAssociations 配置
-	const config = vscode.workspace.getConfiguration("workbench");
-	const configKey = 'editorAssociations'
-	const editorAssociations = config.get(configKey)
-	// 如果 editorAssociations 配置定义了下面的模式，则修改为使用默认 markdown 编辑器打开
-	const key = '{git,gitlens,git-graph}:/**/*.{md,csv,svg}'
-	if (!editorAssociations[key]) {
-		const oldKey = '{git,gitlens}:/**/*.{md,csv,svg}'
-		editorAssociations[oldKey] = undefined
-		editorAssociations[key] = 'default'
-		config.update(configKey, editorAssociations, true)
-	}
-}
